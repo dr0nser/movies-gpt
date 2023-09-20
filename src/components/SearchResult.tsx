ceccf3a2e267f0d696cd9e936d38ded28837dc9d
whyucode <dassouvik14@outlook.com>
2023-09-20 18:49:40 +0530

    no result found check in search

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -6,12 +6,17 @@ const SearchResult: React.FunctionComponent<SearchResultProp> = ({
   movies,
 }): JSX.Element => {
   return (
-    movies && (
-      <div className="absolute bottom-0 z-10 bg-gradient-to-t from-black via-black to-transparent w-full">
+    <div className="absolute bottom-0 z-10 bg-gradient-to-t from-black via-black to-transparent w-full">
+      {movies && movies.length > 0 ? (
         <Gallery title="Search Results" cards={movies} />
-        <Footer />
-      </div>
-    )
+      ) : (
+        <p className="text-center py-40 capitalize text-gray-200 font-semibold tracking-tight text-3xl">
+          No Results Found
+        </p>
+      )}
+
+      <Footer />
+    </div>
   );
 };
 
